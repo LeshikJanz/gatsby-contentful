f04efd2aca7ab317890592871daafdf4fd86f400
leshikjanz <alex.tereshko@softeq.com>
2017-08-08 00:35:11 +0300

    Implemented simple Main class

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,6 @@ module.exports = {
         accessToken: `0efc7c66a824a799f18d037b6bc3ca92209422b0d09ffdebd716a06b8a2a86cf`,
       },
     },
-    `gatsby-transformer-remark`,
+    `gatsby-transformer-remark`,`gatsby-plugin-sass`
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,77 +3,52 @@ import Link from "gatsby-link"
 import * as PropTypes from "prop-types"
 import { rhythm } from "../utils/typography"
 
+import styles from './styles/main.module.scss';
+
 const propTypes = {
   data: PropTypes.object.isRequired,
 }
 
-const Product = ({ node }) =>
-  <div key={node.id}>
-    <Link
-      style={{ color: `inherit`, textDecoration: `none` }}
-      to={`/products/${node.id}/`}
-    >
-      <div
-        style={{
-          display: `flex`,
-          alignItems: `center`,
-          borderBottom: `1px solid lightgray`,
-          paddingBottom: rhythm(1 / 2),
-          marginBottom: rhythm(1 / 2),
-        }}
-      >
-        <div style={{ marginRight: rhythm(1 / 2) }}>
-          {node.image[0].responsiveResolution.src &&
-            <img
-              style={{ margin: 0 }}
-              width={node.image[0].responsiveResolution.width}
-              height={node.image[0].responsiveResolution.height}
-              src={node.image[0].responsiveResolution.src}
-              srcSet={node.image[0].responsiveResolution.srcSet}
-            />}
-        </div>
-        <div style={{ flex: 1 }}>
-          {node.productName.productName}
-        </div>
-      </div>
-    </Link>
-  </div>
+const Navigation = ({mainNavigation}) => (
+  <nav>
+    {
+      mainNavigation.map((nav, i) =>
+        <Link key={i} to={ nav.link } className={styles.navTab}>{nav.label}</Link>)
+    }
+  </nav>
+);
 
 class IndexPage extends React.Component {
+
+  handleMeasure = () => console.log('handleMeasure');
+
+  handleSatisfaction = () => console.log('handleSatisfaction');
+
   render() {
-    console.log('this.props.data');
-    console.log(this.props.data);
+    const {
+      mainNavigation, mainTitle, mainDescription: {mainDescription}, mainButtons
+    } = this.props.data.main.edges[0].node;
 
     return (
-      <h1>App works</h1>
-    // const usProductEdges = this.props.data.us.edges
-    // const deProductEdges = this.props.data.german.edges
-    // return (
-    //   <div style={{ marginBottom: rhythm(2) }}>
-    //     <h2>Gatsby's integration with the Contentful Image API</h2>
-    //     <Link to="/image-api/">See examples</Link>
-    //     <br />
-    //     <br />
-    //     <br />
-    //     <h2>Localization</h2>
-    //     <p>
-    //       The <code>gatsby-source-contentful</code> plugin offers full support
-    //       for Contentful's localization features. Our sample space includes
-    //       products localized into both English and German.
-    //     </p>
-    //     <p>
-    //       An entry and asset node are created for each locale following fallback
-    //       rules for missing localization. In addition, each node has an
-    //       additional field added, <code>node_locale</code> so you can select for
-    //       nodes from a single locale
-    //     </p>
-    //     <h3>en-US</h3>
-    //     {usProductEdges.map(({ node }, i) => <Product node={node} />)}
-    //     <br />
-    //     <br />
-    //     <h3>de</h3>
-    //     {deProductEdges.map(({ node }, i) => <Product node={node} />)}
-    //   </div>
+      <div className={styles.mainContainer}>
+        <div className={styles.header}>
+          <h1>LOGO</h1>
+          <Navigation mainNavigation={mainNavigation}/>
+        </div>
+        <div className={styles.welcome}>
+          <div className={styles.mainTitle}>{mainTitle}</div>
+          <br/>
+          <div className={styles.mainDescription}>{mainDescription}</div>
+        </div>
+        <br/><br/>
+
+        {/* Main Buttons */}
+        <div>
+          {
+            mainButtons.map((b, i) => <button key={i} onClick={this[b.callback]}>{b.title}</button>)
+          }
+        </div>
+      </div>
     )
   }
 }
@@ -110,6 +85,7 @@ export const pageQuery = graphql`
           }
           mainDescription {
             id
+            mainDescription
           }
         }
       }
