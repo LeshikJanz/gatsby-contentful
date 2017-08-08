c59efa4e684c399f4e1cdde60053e4c97d5704ed
LeshikJanz <treshaalesha@gmail.com>
2017-08-08 18:42:29 +0300

    Feat: added All Winners button

--- src/pages/homepage/TopWinners.js
+++ src/pages/homepage/TopWinners.js
@@ -2,21 +2,19 @@ import React from "react"
 import styles from './styles/winner.module.scss'
 import { Winner } from './Winner'
 
-export const TopWinners = ({ winners }) => {
-  console.log('winners');
-  console.log(winners);
-
-  return (
-    <div className={styles.topWinnersContainer}>
-      <div className={styles.winnerLabel}>
-        <h2>Winners 2017</h2>
-        <hr/>
-      </div>
-      <div className={styles.winners}>
-        {
-          winners.map((w, i) => <Winner key={i} winner={w.node}/>)
-        }
-      </div>
+export const TopWinners = ({ winners }) => (
+  <div className={styles.topWinnersContainer}>
+    <div className={styles.winnerLabel}>
+      <h2>Winners 2017</h2>
+      <hr/>
     </div>
-  );
-}
+    <div className={styles.winners}>
+      {
+        winners.map((w, i) => <Winner key={i} winner={w.node}/>)
+      }
+    </div>
+    <div style={{ textAlign: 'center' }}>
+      <button className={styles.winnerButtonWhite}>ALL WINNERS</button>
+    </div>
+  </div>
+);
