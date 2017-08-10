1715d4c177f7437527ca63e849e06872dc83e207
LeshikJanz <treshaalesha@gmail.com>
2017-08-10 19:20:38 +0300

    Feat: created custom Select

--- src/pages/winners/index.js
+++ src/pages/winners/index.js
@@ -3,49 +3,51 @@ import LatestNews from '../../components/LatestNews';
 import Following from '../../components/Following';
 import styles from './winners.module.scss';
 import Select from 'react-select';
+import { withState } from 'recompose';
 
-const Winners = ({ data: { winners, latestNews } }) => {
-  const categories = [
-    { id: 1, value: 'ALL CATEGORIES', label: 'ALL CATEGORIES' },
-    { id: 2, value: 'OVERALL DEVELOPER SATISFACTION', label: 'OVERALL DEVELOPER SATISFACTION' },
-    { id: 3, value: 'BEST HACKATHON', label: 'BEST HACKATHON' },
-    { id: 4, value: 'MACHINE LEARNING Tech media popularity', label: 'MACHINE LEARNING Tech media popularity' },
-    { id: 5, value: 'MACHINE LEARNING FRAMEWORKS', label: 'MACHINE LEARNING FRAMEWORKS' },
-    { id: 6, value: 'PLATFORM AS A SERVICE', label: 'PLATFORM AS A SERVICE' }
-  ];
+const enhance = withState('dropdownValue', 'handleSelect', { value: 'All Categories', label: 'All Categories' });
 
-  const handleCategories = ({ target }) => {
-    console.log('target');
-    console.log(target);
-  };
-
-  return (
+const Winners = enhance(({ dropdownValue, handleSelect, data: { winners, latestNews, categories } }) => (
+  <div>
+    <h1>That is Winners page</h1>
     <div>
-      <h1>That is Winners page</h1>
-      <div>
-        <Select
-          searchable={false}
-          options={categories}
-          onChange={handleCategories}
-        />
-      </div>
-      <div className={styles.info}>
-        <LatestNews latestNews={latestNews.edges}/>
-        <Following />
-      </div>
+      <Select
+        name="Select Category"
+        value={dropdownValue}
+        placeholder=""
+        options={categories.edges.map(e => e.node)}
+        onChange={handleSelect}
+      />
     </div>
-  );
-}
+    <div className={styles.info}>
+      <LatestNews latestNews={latestNews.edges}/>
+      <Following />
+    </div>
+  </div>
+));
 
 export default Winners;
 
 export const pageQuery = graphql`
   query WinnersQuery {
-    winners:  allContentfulFinalists(filter: { isWinner: {eq: true} }) {
+   categories: allContentfulCategory(limit: 1000) {
+    edges {
+      node {
+        id
+        label
+        value: label
+        }
+      }
+    }
+    winners: allContentfulFinalists(filter: { isWinner: {eq: true} }) {
     edges {
       node { 
         id
         companyName
+        companyDescription {
+          id
+          companyDescription
+        }
         logo {
           file {
             url
@@ -73,4 +75,3 @@ export const pageQuery = graphql`
   }
 }
 `
-
