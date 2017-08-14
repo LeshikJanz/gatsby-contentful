a9f850f42cbb1b1896de9b0730962d71f9c02d2b
LeshikJanz <treshaalesha@gmail.com>
2017-08-14 17:49:53 +0300

    Feat: implemented Winners filter

--- src/pages/winners/components/WinnerCategory.js
+++ src/pages/winners/components/WinnerCategory.js
@@ -3,16 +3,20 @@ import styles from '../styles/style.module.scss';
 import Winner from '../../../components/Winner'
 
 const WinnerCategory = ({ winners }) => (
-  <div className={styles.winnerCategoryContainer}>
-    <div className={styles.categoryLabel}>
-      <h2>{winners && winners[0].node.category.label}</h2>
-      <hr/>
-    </div>
-    <div className={styles.winners}>
-      {
-        winners && winners.map((w, i) => <Winner key={i} winner={w.node} styles={styles}/>)
-      }
-    </div>
+  <div>
+    {!!winners.length &&
+      <div className={styles.winnerCategoryContainer}>
+        <div className={styles.categoryLabel}>
+          <h2>{winners && winners[0].node.category.label}</h2>
+          <hr/>
+        </div>
+        <div className={styles.winners}>
+          {
+            winners && winners.map((w, i) => <Winner key={i} winner={w.node} styles={styles}/>)
+          }
+        </div>
+      </div>
+    }
   </div>
 )
 
--- src/pages/winners/index.js
+++ src/pages/winners/index.js
@@ -4,48 +4,52 @@ import Following from '../../components/Following';
 import styles from './styles/style.module.scss';
 import Select from 'react-select';
 import WinnerCategory from './components/WinnerCategory';
-import { compose, withState } from 'recompose';
+import { compose, withState, withHandlers, lifecycle, withProps } from 'recompose';
+import DEFAULT_CATEGORY_FILTER from "./contants/index";
 
 const Winners = (props) => {
   const {
-    dropdownCategoryValue, dropdownYearValue, handleCategorySelect, handleYearSelect,
-    data: {winners, latestNews, categories}
+    handleCategorySelect, handleDateSelect, selectedCategory, selectedYear, selectedWinners,
+    data: { winners, latestNews, categories }
   } = props;
 
   const getYears = () =>
+    // Make year unique
     [...new Set(categories.edges.map(e => e.node.date.split('-')[0]))]
       .map(y => ({
         value: y,
         label: y
       }));
 
+  const getWinners = (category) =>
+    (selectedWinners || winners.edges).filter(w => w.node.category.label == category.node.label);
+
   return (
     <div>
       <div className={styles.dropdownsContainer}>
         <Select
           name="Select Year"
-          value={dropdownYearValue}
-          style={{minWidth: '98px'}}
+          value={selectedYear}
+          style={{ minWidth: '98px' }}
           placeholder=""
           options={getYears()}
-          onChange={handleYearSelect}
+          onChange={handleDateSelect}
         />
         <Select
           name="Select Category"
-          value={dropdownCategoryValue}
-          style={{minWidth: '355px'}}
+          clearable={false}
+          value={selectedCategory}
+          style={{ minWidth: '355px' }}
           placeholder=""
           options={categories.edges.map(e => e.node)}
           onChange={handleCategorySelect}
         />
       </div>
-      <div>
-        {
-          categories.edges.map((c, i) =>
-            <WinnerCategory key={i}
-                            winners={winners.edges.filter(w => w.node.category.label === c.node.label)}/>)
-        }
-      </div>
+      {
+        categories.edges.map((c, i) =>
+          <WinnerCategory key={i}
+                          winners={getWinners(c)}/>)
+      }
       <div className={styles.info}>
         <LatestNews latestNews={latestNews.edges}/>
         <Following />
@@ -55,9 +59,60 @@ const Winners = (props) => {
 };
 
 export default compose(
-  withState('dropdownCategoryValue', 'handleCategorySelect', {value: 'All Categories', label: 'All Categories'}),
-  withState('dropdownYearValue', 'handleYearSelect', {value: '2017', label: '2017'})
-)(Winners);
+  withState('selectedCategory', 'setCategory', { value: 'All Categories', label: 'All Categories' }),
+  withState('selectedYear', 'setYear', { value: '2017', label: '2017' }),
+  withState('selectedWinners', 'setWinners', null),
+  withProps((props) => ({
+    runAllCategoriesFilter: (date) => {
+      props.setWinners(props.data.winners.edges.filter(({ node: { category } }) =>
+      category.date.split('-')[0] == date));
+    },
+    runFilter: (item, categoryFilter, dateFilter) => {
+      return item.label === categoryFilter && item.date.split('-')[0] == dateFilter
+    }
+  })),
+  withHandlers({
+    handleCategorySelect: (props) => (evt) => {
+      props.setCategory(evt);
+      if (evt.label === 'All Categories') {
+        props.runAllCategoriesFilter(props.selectedYear.label);
+        return;
+      }
+
+      // Filtering out winners by category
+      const filteredWinners = props.data.winners.edges.filter(
+        ({ node: { category } }) => props.runFilter(category, evt.label, props.selectedYear.label)
+      );
+
+      props.setWinners(filteredWinners);
+    },
+
+    handleDateSelect: (props) => (evt) => {
+      props.setYear(evt);
+
+      if (props.selectedCategory.label === 'All Categories') {
+        props.runAllCategoriesFilter(evt.label);
+        return;
+      }
+
+      // Filtering out winners by category
+      const filteredWinners = props.data.winners.edges.filter(
+        ({ node: { category } }) => props.runFilter(category, props.selectedCategory.label, evt.label)
+      );
+
+      // Filtering out winners by year
+      props.setWinners(filteredWinners);
+    }
+  }),
+  lifecycle({
+    componentDidMount() {
+      this.props.data.categories.edges.unshift({ node: DEFAULT_CATEGORY_FILTER });
+      this.props.handleCategorySelect(this.props.selectedCategory);
+      this.props.handleDateSelect(this.props.selectedYear);
+    }
+  })
+)
+(Winners);
 
 export const pageQuery = graphql`
   query WinnersQuery {
