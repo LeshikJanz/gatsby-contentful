33bc64f28cf7bb2334d6af24a4781fd492451105
LeshikJanz <treshaalesha@gmail.com>
2017-08-15 14:58:51 +0300

    Refactor: refactored FinalistCategory

--- src/modules/finalists/components/FinalistCategory.js
+++ src/modules/finalists/components/FinalistCategory.js
@@ -1,29 +1,24 @@
 import React from 'react';
 import styles from '../styles/style.module.scss';
 
-const FinalistCategory = ({ winners }) => {
-  console.log('winners');
-  console.log(winners);
-
-  return(
-    <div>
-      {winners && !!winners.length &&
-      <div className={styles.winnerCategoryContainer}>
-        <div className={styles.categoryLabel}>
-          <h2>{winners && winners[0].node.category.label}</h2>
-          <hr/>
-        </div>
-        <div className={styles.winners}>
-          {
-            winners && winners
-              .sort((a,b) => a.node.medal.order - b.node.medal.order)
-              .map((w, i) => <div className={styles.winnerItem} key={i}>{w.node.companyName}</div>)
-          }
-        </div>
+const FinalistCategory = ({ winners }) => (
+  <div>
+    {winners && !!winners.length &&
+    <div className={styles.winnerCategoryContainer}>
+      <div className={styles.categoryLabel}>
+        <h2>{winners && winners[0].node.category.label}</h2>
+        <hr/>
       </div>
-      }
+      <div className={styles.winners}>
+        {
+          winners && winners
+            .sort((a, b) => a.node.medal.order - b.node.medal.order)
+            .map((w, i) => <div className={styles.winnerItem} key={i}>{w.node.companyName}</div>)
+        }
+      </div>
     </div>
-  )
-}
+    }
+  </div>
+);
 
 export default FinalistCategory;
