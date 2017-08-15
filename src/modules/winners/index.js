6140f8c52e61e9ae5cac6da019614ab1673353c9
LeshikJanz <treshaalesha@gmail.com>
2017-08-15 13:15:21 +0300

    Refactor: refactored winners filtering

--- src/modules/winners/index.js
+++ src/modules/winners/index.js
@@ -5,24 +5,26 @@ import styles from './styles/style.module.scss';
 import Select from 'react-select';
 import WinnerCategory from './components/WinnerCategory';
 import { compose, withState, withHandlers, lifecycle, withProps } from 'recompose';
-import { DEFAULT_CATEGORY_FILTER } from "./constants/index";
+
+const DEFAULT_DATE_FILTER = { value: '', label: 'All Years' };
+const DEFAULT_CATEGORY_FILTER = { value: '', label: 'All Categories' };
 
 const Winners = (props) => {
   const {
     handleCategorySelect, handleDateSelect, selectedCategory, selectedYear, selectedWinners,
-    data: {winners, latestNews, categories}
+    data: { latestNews, categories }
   } = props;
 
   const getYears = () =>
-    // Make year unique
-    [...new Set(categories.edges.map(e => e.node.date.split('-')[0]))]
-      .map(y => ({
-        value: y,
-        label: y
-      }));
+    [DEFAULT_DATE_FILTER,
+      ...[...new Set(categories.edges.map(e => e.node.date.split('-')[0]))]
+        .map(y => ({
+          value: y,
+          label: y
+        }))
+    ];
 
-  const getWinners = (category) =>
-    (selectedWinners || winners.edges).filter(w => w.node.category.label == category.node.label);
+  const getCategories = () => [DEFAULT_CATEGORY_FILTER, ...categories.edges.map(e => e.node)]
 
   return (
     <div>
@@ -30,7 +32,8 @@ const Winners = (props) => {
         <Select
           name="Select Year"
           value={selectedYear}
-          style={{minWidth: '98px', marginTop: '20px'}}
+          clearable={false}
+          style={{ minWidth: '98px' }}
           placeholder=""
           options={getYears()}
           onChange={handleDateSelect}
@@ -39,21 +42,17 @@ const Winners = (props) => {
           name="Select Category"
           clearable={false}
           value={selectedCategory}
-          style={{minWidth: '355px', marginTop: '20px'}}
+          style={{ minWidth: '355px' }}
           placeholder=""
-          options={categories.edges.map(e => e.node)}
+          options={getCategories()}
           onChange={handleCategorySelect}
         />
       </div>
       {
-        categories.edges.map((c, i) =>
-          <WinnerCategory key={i}
-                          winners={getWinners(c)}/>)
-      }
-      {
-        selectedWinners && !selectedWinners.length &&
-        <h1 className={styles.mainContainer}>There is no one winner
-          for {selectedCategory.label + ' ' + selectedYear.label}</h1>
+        getCategories()
+          .filter(c => selectedCategory === DEFAULT_CATEGORY_FILTER ? true : c === selectedCategory)
+          .map((c, i) => <WinnerCategory key={i}
+                                         winners={selectedWinners.filter(({ node: { category } }) => category.label === c.label)}/>)
       }
       <div className={styles.info}>
         <LatestNews latestNews={latestNews.edges}/>
@@ -64,60 +63,26 @@ const Winners = (props) => {
 };
 
 export default compose(
-  withState('selectedCategory', 'setCategory', {value: 'All Categories', label: 'All Categories'}),
-  withState('selectedYear', 'setYear', {value: '2017', label: '2017'}),
-  withState('selectedWinners', 'setWinners', null),
+  withState('selectedCategory', 'setCategory', DEFAULT_CATEGORY_FILTER),
+  withState('selectedYear', 'setYear', DEFAULT_DATE_FILTER),
   withProps((props) => ({
-    runAllCategoriesFilter: (date) => {
-      props.setWinners(props.data.winners.edges.filter(({node: {category}}) =>
-      category.date.split('-')[0] == date));
-    },
-    runFilter: (item, categoryFilter, dateFilter) => {
-      return item.label === categoryFilter && item.date.split('-')[0] == dateFilter
-    }
+    winners: props.data.winners.edges,
   })),
+  withProps((props) => {
+    console.log(props);
+    return ({
+      selectedWinners: props.winners.filter(
+        ({ node: { category } }) => ( category.label === props.selectedCategory.label || props.selectedCategory.value === DEFAULT_CATEGORY_FILTER.value) &&
+        ( category.date.split('-')[0] == props.selectedYear.label || props.selectedYear.value === DEFAULT_DATE_FILTER.value ))
+    })
+  }),
   withHandlers({
     handleCategorySelect: (props) => (evt) => {
       props.setCategory(evt);
-      if (evt.label === 'All Categories') {
-        props.runAllCategoriesFilter(props.selectedYear.label);
-        return;
-      }
-
-      // Filtering out winners by category
-      const filteredWinners = props.data.winners.edges.filter(
-        ({node: {category}}) => props.runFilter(category, evt.label, props.selectedYear.label)
-      );
-
-      props.setWinners(filteredWinners);
     },
-
     handleDateSelect: (props) => (evt) => {
       props.setYear(evt);
-
-      if (props.selectedCategory.label === 'All Categories') {
-        props.runAllCategoriesFilter(evt.label);
-        return;
-      }
-
-      // Filtering out winners by category
-      const filteredWinners = props.data.winners.edges.filter(
-        ({node: {category}}) => props.runFilter(category, props.selectedCategory.label, evt.label)
-      );
-
-      // Filtering out winners by year
-      props.setWinners(filteredWinners);
-    }
-  }),
-  lifecycle({
-    componentDidMount() {
-      this.props.data.categories.edges.unshift({node: DEFAULT_CATEGORY_FILTER});
-      this.props.handleCategorySelect(this.props.selectedCategory);
-      this.props.handleDateSelect(this.props.selectedYear);
     },
-    componentWillUnmount() {
-      this.props.data.categories.edges.shift();
-    }
   })
 )
 (Winners);
@@ -155,7 +120,6 @@ export const pageQuery = graphql`
         }
         medal {
           label
-          order
         }
     }
     }
