c571f6b9e11c3f785b7f04d8a532ee627185e860
LeshikJanz <treshaalesha@gmail.com>
2017-08-16 17:56:19 +0300

    Fix: deleted press markups for debugging

--- src/modules/press/index.js
+++ src/modules/press/index.js
@@ -11,47 +11,47 @@ import googleIcon from '../../assets/images/icons/google-plus.svg';
 
 const Press = ({ data: { press, latestNews } }) => (
   <div>
-    <div className={styles.mainContainer}>
-      <div className={styles.categoryLabel}>
-        <h2>Press Releases</h2>
-        <hr/>
-      </div>
-      <div className={styles.pressItems}>
-        {
-          press.edges.map(({ node }, i) => (
-            <div key={i}>
-              <div className={styles.pressContainer}>
-                <div>
-                  {/*<div className={styles.date}>{format(new Date(node.date), 'MM DD,YYYY')}</div>*/}
-                  <div className={styles.title}>{node.title}</div>
-                  <div className={styles.description}>{node.description.description}</div>
-                </div>
-                <div className={styles.actions}>
-                  <div className={styles.pdfDownload}>
-                    <h4>Download</h4>
-                    <a target="_blank" href={node.pdf.document.file.url}>{node.pdf.document.file.fileName}</a>
-                  </div>
-                  <div className={styles.shareActions}>
-                    <h4>Share</h4>
-                    <div className={styles.followIcons}>
-                      <img src={twitterIcon} alt="Twitter"/>
-                      <img src={linkedin} alt="LinkedIn"/>
-                      <img src={facebook} alt="Facebook"/>
-                      <img src={googleIcon} alt="Google"/>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <hr/>
-            </div>
-          ))
-        }
-      </div>
-    </div>
-    <div className={styles.info}>
-      <LatestNews latestNews={latestNews.edges}/>
-      <Following />
-    </div>
+    {/*<div className={styles.mainContainer}>*/}
+      {/*<div className={styles.categoryLabel}>*/}
+        {/*<h2>Press Releases</h2>*/}
+        {/*<hr/>*/}
+      {/*</div>*/}
+      {/*<div className={styles.pressItems}>*/}
+        {/*{*/}
+          {/*press.edges.map(({ node }, i) => (*/}
+            {/*<div key={i}>*/}
+              {/*<div className={styles.pressContainer}>*/}
+                {/*<div>*/}
+                  {/*/!*<div className={styles.date}>{format(new Date(node.date), 'MM DD,YYYY')}</div>*!/*/}
+                  {/*<div className={styles.title}>{node.title}</div>*/}
+                  {/*<div className={styles.description}>{node.description.description}</div>*/}
+                {/*</div>*/}
+                {/*<div className={styles.actions}>*/}
+                  {/*<div className={styles.pdfDownload}>*/}
+                    {/*<h4>Download</h4>*/}
+                    {/*<a target="_blank" href={node.pdf.document.file.url}>{node.pdf.document.file.fileName}</a>*/}
+                  {/*</div>*/}
+                  {/*<div className={styles.shareActions}>*/}
+                    {/*<h4>Share</h4>*/}
+                    {/*<div className={styles.followIcons}>*/}
+                      {/*<img src={twitterIcon} alt="Twitter"/>*/}
+                      {/*<img src={linkedin} alt="LinkedIn"/>*/}
+                      {/*<img src={facebook} alt="Facebook"/>*/}
+                      {/*<img src={googleIcon} alt="Google"/>*/}
+                    {/*</div>*/}
+                  {/*</div>*/}
+                {/*</div>*/}
+              {/*</div>*/}
+              {/*<hr/>*/}
+            {/*</div>*/}
+          {/*))*/}
+        {/*}*/}
+      {/*</div>*/}
+    {/*</div>*/}
+    {/*<div className={styles.info}>*/}
+      {/*<LatestNews latestNews={latestNews.edges}/>*/}
+      {/*<Following />*/}
+    {/*</div>*/}
   </div>
 );
 
