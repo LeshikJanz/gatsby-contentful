cee464af576a6211e586cfc5ae5c8349c29db406
LeshikJanz <treshaalesha@gmail.com>
2017-08-16 17:32:08 +0300

    Fix: added about page

--- src/modules/about/index.js
+++ src/modules/about/index.js
@@ -1,10 +1,77 @@
 import React from 'react';
+import LatestNews from '../../components/LatestNews';
+import Following from '../../components/Following';
+import styles from './styles/style.module.scss';
+const classNames = require('classnames/bind');
+const cx = classNames.bind(styles);
 
-export default class About extends React.Component {
+const About = ({ data: { content, latestNews } }) => {
 
-  render() {
-    return (
-      <h1>That is About page</h1>
-    )
+  const {
+    title, description: { description }, companyDescription: {companyDescription},
+    companyName, companyWebSite, picture
+  } = content.edges[ 0 ].node;
+
+  return (
+    <div>
+      <div className={cx(['mainContainer', 'welcome'])}>
+        <div className={styles.welcomeText}>
+          <div className={styles.title}>{title}</div>
+          <br/>
+          <div className={styles.description}>{description}</div>
+          <h2>About {companyName}</h2>
+          <div className={styles.description}>{companyDescription}</div>
+          <a href={companyWebSite} />
+        </div>
+        <div className={styles.welcomePicture}>
+          <img src={picture.file.url}/>
+          <div className={styles.actions}>
+            <button className={styles.winnerButton}>GET IN TOUCH</button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.info}>
+        <LatestNews latestNews={latestNews.edges}/>
+        <Following />
+      </div>
+    </div>
+  )
+};
+
+export default About;
+
+export const pageQuery = graphql`
+  query AboutQuery {
+   content: allContentfulAbout(limit: 1000) {
+    edges {
+      node { 
+        id
+        title
+        description {
+          description
+        }
+        picture {
+          id
+          file {
+            url
+          }
+        }
+        companyName
+        companyDescription {
+          companyDescription
+        }
+        companyWebSite
+    }
+    }
+    }
+    latestNews: allContentfulNews(limit: 1000) {
+    edges {
+      node {
+        date
+        headLine
+        reporter
+    }
+  }
   }
 }
+`
