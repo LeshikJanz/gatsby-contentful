790601ef1e81bc71f9ed11b64f423a72d18f0cb8
LeshikJanz <treshaalesha@gmail.com>
2017-08-22 17:15:22 +0300

    Feat: added homepage logo field to the Finalists model

--- src/pages/index.js
+++ src/pages/index.js
@@ -82,6 +82,11 @@ export const pageQuery = graphql`
             url
           }
         }
+        homepageLogo {
+          file {
+            url
+          }
+        }
         webSite
         category {
           label
